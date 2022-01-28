17e32688b8aa70cb65bdd4256b082c2ed54b5dff
YenaChoi00 <choiyn33@naver.com>
2022-01-28 14:47:35 +0900

    Add Blog Page

---
+++ resources/views/blog/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="w-4/5 m-auto text-left">
+        <div class="py-15">
+            <h1 class="text-6xl">
+                Create Post
+            </h1>
+        </div>
+    </div>
+
+    
+
+    
+
+@endsection
