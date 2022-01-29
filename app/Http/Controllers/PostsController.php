8af459cf2d2f1c7fd161867e8586f02aa6a0ae06
YenaChoi00 <choiyn33@naver.com>
2022-01-30 00:01:51 +0900

    Add slug service

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Cviebrock\EloquentSluggable\Services\SlugService;
 
 class PostsController extends Controller
 {
@@ -19,7 +20,7 @@ class PostsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create()        // 작성 페이지 보여줌
     {
         return view('blog.create');
     }
@@ -30,9 +31,27 @@ class PostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request) // 입력된 데이터 저장
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'image'=>'required|mimes:jpg,png,jpeg|max:5048'
+        ]);
+
+        $newImageName = uniqid() . '-' . $request->title. '.' . $request->image->extension();       // uniqid(): 고유 id 생성 메소드
+        $request->image->move(public_path('images'), $newImageName);    // /images 라는 public 폴더 밑에 이미지 저장 -> 폴더 굳이 안만들어도 라라벨이 만들어줌
+
+        Post::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'slug' => SlugService::createSlug(Post::class, 'slug', $request->title),     // 모델, (db의) col 이름, 변환될 것,
+            'image_path' => $newImageName,  // 실제 이미지 업로드x 이미지 이름만 업로드
+            'user_id' => auth()->user()->id
+        ]);
+
+        return redirect('/blog')
+            ->with('message', 'Your post has been added!');
     }
 
     /**
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,13 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
+
+    protected $fillable = ['title', 'slug', 'description', 'image_path', 'user_id'];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function sluggable(): array  // 반환 값은 array형태여야 함
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
